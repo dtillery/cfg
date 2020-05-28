d5f1198ba107449ce02acba48cddc690e9111668
David Tillery <djtillery@gmail.com>
2020-05-28 17:15:26 -0400

    fix bad URL

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,4 +1,4 @@
-git clone --bare git@bitbucket.org:dtillery/cfg.git $HOME/.cfg
+git clone --bare git@github.com:dtillery/cfg.git $HOME/.cfg
 function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
