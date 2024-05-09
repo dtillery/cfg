06652ec24a02630bbf3f12c44a404e5306e3e092
David Tillery <djtillery@gmail.com>
2024-05-08 17:30:50 -0700

    more aliases

--- scripts/zsh/aliases.zsh
+++ scripts/zsh/aliases.zsh
@@ -13,3 +13,9 @@ alias whatismyip="dig +short myip.opendns.com @resolver1.opendns.com"
 
 # iina video player cli
 alias iina-cli='/Applications/IINA.app/Contents/MacOS/iina-cli'
+
+# cat to bat
+alias cat='bat --paging=never'
+
+# mariners
+alias maribers='bitemex mlb -p mariners'
