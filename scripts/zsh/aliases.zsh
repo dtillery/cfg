2e6336588bd8a3fd8bdc653ffbede8cf60bc92a3
David Tillery <djtillery@gmail.com>
2024-11-12 14:18:37 -0800

    add iina-plugin alias

--- scripts/zsh/aliases.zsh
+++ scripts/zsh/aliases.zsh
@@ -13,6 +13,7 @@ alias whatismyip="dig +short myip.opendns.com @resolver1.opendns.com"
 
 # iina video player cli
 alias iina-cli='/Applications/IINA.app/Contents/MacOS/iina-cli'
+alias iina-plugin='/Applications/IINA.app/Contents/MacOS/iina-plugin'
 
 # cat to bat
 alias cat='bat --paging=never'
