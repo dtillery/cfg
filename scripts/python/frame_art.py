d651b7886d758941e178cfee82788142f6e32028
David Tillery <djtillery@gmail.com>
2025-11-06 09:25:43 -0800

    update frame_art.py and uv cheat

--- scripts/python/frame_art.py
+++ scripts/python/frame_art.py
@@ -12,17 +12,17 @@
 # uv run frame_art.py info
 
 import asyncio
-from typing import Annotated, TYPE_CHECKING
+import csv
+from pathlib import PosixPath
 
-from cyclopts import App, Parameter
+from cyclopts import App
 from cyclopts.types import ExistingDirectory, File
 from samsungtvws.async_art import SamsungTVAsyncArt
 
-if TYPE_CHECKING:
-    from pathlib import PosixPath
-
 FRAME_IP: str = "192.168.0.83"
 UPLOADED_CATEGORY_ID: str = "MY-C0002"
+UPLOAD_TIMEOUT: int = 60
+VALID_FILETYPES = ("jpg", "jpeg")
 
 app = App()
 
@@ -41,7 +41,9 @@ async def info() -> None:
             available_art = await tv.available()
             print(f"# Available Art: {len(available_art)}")
             for art in available_art:
-                print(f"\t{art['content_id']} ({art['category_id']}: {art.get('matte_id')})")
+                print(
+                    f"\t{art['content_id']} ({art['category_id']}: {art.get('matte_id')})"
+                )
             current_art = await tv.get_current()
             print(f"Current Art: {current_art}")
             art_settings = await tv.get_artmode_settings()
@@ -62,7 +64,11 @@ async def delete() -> None:
     await tv.start_listening()
     try:
         all_art = await tv.available()
-        content_ids = [art["content_id"] for art in all_art if art["category_id"] == UPLOADED_CATEGORY_ID]
+        content_ids = [
+            art["content_id"]
+            for art in all_art
+            if art["category_id"] == UPLOADED_CATEGORY_ID
+        ]
         await tv.delete_list(content_ids)
     except Exception as e:
         print(f"Error: {e}")
@@ -70,33 +76,100 @@ async def delete() -> None:
         await tv.close()
 
 
+def get_valid_filepaths(src_dir: PosixPath) -> list[PosixPath]:
+    filepaths: list[PosixPath] = []
+    for filetype in VALID_FILETYPES:
+        filepaths.extend(src_dir.glob(f"*.{filetype}", case_sensitive=False))
+    return filepaths
+
+
+async def upload_to_tv(
+    tv: SamsungTVAsyncArt, path: PosixPath, matte: str = "none"
+) -> str:
+    with open(path, "rb") as f:
+        data = f.read()
+    file_type = path.suffix[1:]
+    content_id = await tv.upload(
+        data, file_type=file_type, matte=matte, timeout=UPLOAD_TIMEOUT
+    )
+    if not content_id:
+        print(f"WARNING: No content_id returned, probable timeout for {path.name}")
+    return content_id
+
+
 @app.command
-async def upload(sync_dir: ExistingDirectory ) -> None:
+async def upload(sync_dir: ExistingDirectory) -> None:
     tv = SamsungTVAsyncArt(FRAME_IP)
     await tv.start_listening()
     try:
-        valid_filetypes = ("*.jpg", "*.jpeg")
-        filepaths: list[PosixPath] = []
-        for files in valid_filetypes:
-            filepaths.extend(sync_dir.glob(files, case_sensitive=False))
-
-        for filepath in filepaths:
-            print(f"Uploading {filepath}...")
-            with open(filepath, "rb") as f:
-                data = f.read()
-            file_type = filepath.suffix[1:]
-            content_id = await tv.upload(data, file_type=file_type, matte="none")
+        filepaths = get_valid_filepaths(sync_dir)
+        for i, filepath in enumerate(filepaths, start=1):
+            print(f"Uploading #{i}: {filepath}...")
+            content_id = await upload_to_tv(tv, filepath)
             print(f"Uploaded as {content_id}")
     except Exception as e:
-        print(f"Error: {e}")
+        print(f"Error:")
+        print(e)
     finally:
         await tv.close()
 
 
 @app.command
 async def sync(sync_dir: ExistingDirectory, sync_file: File) -> None:
-    print(sync_file)
+    sync_file.touch(exist_ok=True)
 
+    # read/create synced file records
+    synced_file_records: dict[str, str] = {}
+    with open(sync_file, "r") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            synced_file_records[row["filename"]] = row["content_id"]
+
+    # get all valid files in sync dir
+    filepaths = get_valid_filepaths(sync_dir)
+
+    # determine content_ids to delete
+    filenames = [p.name for p in filepaths]
+    content_ids_to_delete: list[str] = []
+    for synced_filename in list(synced_file_records.keys()):
+        if synced_filename not in filenames:
+            content_ids_to_delete.append(synced_file_records.pop(synced_filename))
+
+    # determine new files to add
+    files_to_upload: list[PosixPath] = []
+    for filepath in filepaths:
+        if filepath.name not in synced_file_records:
+            files_to_upload.append(filepath)
+
+    print(f"# Files to upload: {len(files_to_upload)}")
+    print(f"# Files to delete: {len(content_ids_to_delete)}")
+
+    tv = SamsungTVAsyncArt(FRAME_IP)
+    await tv.start_listening()
+    try:
+        # delete content_ids
+        print(f"Deleting Content with IDs: {content_ids_to_delete}")
+        await tv.delete_list(content_ids_to_delete)
+
+        # upload new files
+        for i, filepath in enumerate(files_to_upload, start=1):
+            print(f"Uploading #{i}: {filepath}...")
+            content_id = await upload_to_tv(tv, filepath)
+            if content_id:
+                print(f"Uploaded as {content_id}")
+                synced_file_records[filepath.name] = content_id
+    except Exception as e:
+        print(f"Error:")
+        print(e)
+    finally:
+        await tv.close()
+
+    # write updated sync records
+    with open(sync_file, "w") as f:
+        writer = csv.DictWriter(f, fieldnames=["filename", "content_id"])
+        writer.writeheader()
+        for filename, content_id in synced_file_records.items():
+            writer.writerow({"filename": filename, "content_id": content_id})
 
 
 if __name__ == "__main__":
